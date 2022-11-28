7da1b1b7e0cbe0d91ade4be7cdbfe7a227d10c36
Turdiyeva Feruza <feruzaturdiyevashuhrat@gmail.com>
2022-11-28 17:54:11 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/*document
+.querySelector("button")
+.addEventListener("click", () => +prompt("To'lov summasini kiriting")
+)*/
+alert("ASSALOMU ALEYKUM!TUR KOMPANIYAMIZ ELEKTRON SAYTIGA HUSH KELIBSIZ");
+confirm(`Siz to'lovni amalga oshirishga tayyormisiz?`);
+alert("Bilet-3937500 so'm \n Yashash harajatlari-7650000 so'm\n Sayr qilish-2958720 so'm \n Umumiy mablag'-14546220 so'm");
+let allCosts = 14546220;
+let userMoney = +prompt("Sayohat uchun mablag'ingizni kiriting:");
+
+if(userMoney>=allCosts){
+    alert("Sizning mablag'ingiz sayohat uchun yetarli.\n Oq yo'l!!!");
+    document
+.querySelector("button")
+    console.log(alert);
+}else if(userMoney<allCosts){
+    alert("Sizning mablag'ingiz yetarli emas.Iltimos,yetarli mablag' bilan 10 kundan keyin yana urinib ko'ring!");
+    console.log(alert);
+}else{
+alert("Mablag'ni to'g'ri kiriting!");
+console.log(alert);
+}
